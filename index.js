dd13d1734d6e7a49019b8da6f5d29a34d3a4b351
PedroMak <pedromakhoul@gmail.com>
2025-06-27 15:56:38 -0300

    added counter

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ var app = express();
 
 
 app.get('/', function(req, res) {
+    counter.labels('200').inc();
 
     res.send('Hello World!');
 
 });
 
+app.get('/metrics', async function(req, res){
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
 
+});
 
 app.listen(3000);
