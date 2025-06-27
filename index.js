92fa136e422155a85e61c5f10c1ea90a3ade38f3
PedroMak <pedromakhoul@gmail.com>
2025-06-27 16:49:31 -0300

    added histogram test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var prom = require('prom-client');
-const register = prom.register;
+const register = prom.register; //registro global de métricas, guarda todas as métricas no formato certo para o prometheus
 
 var app = express();
 
@@ -15,9 +15,16 @@ const gauge = new prom.Gauge({
     help: 'Gauge example'
  });
 
+ const histogram = new prom.Histogram({
+  name: 'request_time_seconds',
+  help: 'Tempo de resposta da API',
+  buckets: [0.1, 0.2, 0.3, 0.4, 0.5],
+});
+
 app.get('/', function(req, res) {
-    counter.labels('200').inc();
-    gauge.set(100*Math.random());
+    counter.labels('200').inc(); // contador com label 200 que incrementa a cada requisição
+    gauge.set(100*Math.random()); // simulando um gauge
+    histogram.observe(Math.random()); // simulando um histograma
 
     res.send('Hello World!');
 
